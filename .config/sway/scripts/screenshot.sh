74da79fbbadec0e1d159219e3fbd41a16e12f697
Casey Walker <kcmw3e@gmail.com>
2025-10-08 21:45:57 -0400

    Simplify screenshot logic

--- .config/sway/scripts/screenshot.sh
+++ .config/sway/scripts/screenshot.sh
@@ -15,37 +15,23 @@ readonly datetime=$(date +%Y-%m-%d_%H-%M-%S)
 readonly output_filename="screenshot-${datetime}.png"
 readonly output_filepath="${HOME}/screenshots/${output_filename}"
 
-function take_fullscreen_screenshot() {
-    grim -t png "${output_filepath}"
-}
 
-function take_region_screenshot() {
-    # Can't be set using `readonly` since that would overwrite the exit status.
-    # I also tried using `readonly` on the line following the exit status
-    # assignment, but that seemed to just overwrite the variable with an empty
-    # value.
-    region=$(slurp)
-    readonly exit_status=$?
-
-    printf "Region: %s\n" "${region}"
-
-    # TODO: add check for `region` being empty
-    if [[ "${exit_status}" == 0 ]] then
-      grim -g "${region}" -t png "${output_filepath}"
-    else
-      exit 1
-    fi
-}
 
 case $1 in
   -r | --region)
-    take_region_screenshot
-    play_shutter_sound
+    readonly region=$(slurp)
   ;;
   *)
-    take_fullscreen_screenshot
-    play_shutter_sound
+    # Nothing to do otherwise
   ;;
 esac
+
+if [[ -v region ]]; then
+  grim -g "${region}" -t png "${output_filepath}"
+else
+  grim -t png "${output_filepath}"
+fi
+
+play_shutter_sound
 
 notify-send "${output_filepath}"
